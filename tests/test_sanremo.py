463b01166213d979aa733af15f9cb4975debde59
Alexis Daboville <alexis.daboville@gmail.com>
2024-06-18 12:55:09 +0100

    fix ci

--- tests/test_sanremo.py
+++ tests/test_sanremo.py
@@ -1,6 +1,7 @@
 import sanremo
+
+
 def test_is_san_remo_interesting_yet():
     # this test should pass most of the year :)
     # again, point of this is to play with packaging
-    assert False
     assert not sanremo.is_san_remo_interesting_yet()
