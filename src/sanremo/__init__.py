9b8b8e0d8b55c5324161cce8a72fa18c5b65150d
Alexis Daboville <alexis.daboville@gmail.com>
2024-10-11 22:21:07 +0100

    overengineered tests because why not

--- src/sanremo/__init__.py
+++ src/sanremo/__init__.py
@@ -1,25 +1,47 @@
 import re
 import sys
+from dataclasses import dataclass
 
 import requests
 from colorama import Fore, Style
 
+URL = "https://ismilansanremoexcitingyet.com/"
 
-def is_san_remo_interesting_yet():
-    response = requests.get("https://ismilansanremoexcitingyet.com/")
 
-    if response.status_code != 200:
-        raise
+def fetch_website(get=requests.get):
+    response = get(URL)
+    response.raise_for_status()
+    return response.text
 
-    interesting_yet = re.search(r"<h2>(Yes|No)</h2>", response.text).group(1).lower()
+
+def is_san_remo_interesting_yet(html):
+    interesting_yet = re.search(r"<h2>(Yes|No)</h2>", html).group(1).lower()
     return interesting_yet == "yes"
 
 
-def main():
-    interesting_yet = is_san_remo_interesting_yet()
+@dataclass
+class Result:
+    status_text: str
+    exit_code: int
+
+
+def process(fetch=fetch_website):
+    try:
+        html = fetch()
+    except Exception:
+        return Result(f"{Fore.RED}❌ probably not?{Style.RESET_ALL}", 2)
+
+    interesting_yet = is_san_remo_interesting_yet(html)
 
     if interesting_yet:
-        print(f"{Fore.GREEN}✅ finally (almost sounds like a bug!){Style.RESET_ALL}")
-    else:
-        print(f"{Fore.RED}❌ what did you expect?{Style.RESET_ALL}")
-        sys.exit(1)
+        return Result(
+            f"{Fore.GREEN}✅ finally (almost sounds like a bug!){Style.RESET_ALL}", 0
+        )
+
+    return Result(f"{Fore.RED}❌ what did you expect?{Style.RESET_ALL}", 1)
+
+
+def main():
+    result = process()
+    print(result.status_text)
+    sys.exit(result.exit_code)
--- tests/test_sanremo.py
+++ tests/test_sanremo.py
@@ -1,7 +1,42 @@
+import functools
+import unittest
+from dataclasses import dataclass
+
 import sanremo
+from requests.exceptions import RequestException
 
 
-def test_is_san_remo_interesting_yet():
-    # this test should pass most of the year :)
-    # again, point of this is to play with packaging
-    assert not sanremo.is_san_remo_interesting_yet()
+class TestSanRemo(unittest.TestCase):
+    def test_is_san_remo_interesting_yet(self):
+        self.assertTrue(sanremo.is_san_remo_interesting_yet("<h2>Yes</h2>"))
+        self.assertFalse(sanremo.is_san_remo_interesting_yet("<h2>No</h2>"))
+
+    def test_process(self):
+        self.assertEqual(sanremo.process(lambda: "<h2>Yes</h2>").exit_code, 0)
+        self.assertEqual(sanremo.process(lambda: "<h2>No</h2>").exit_code, 1)
+
+        def raising_fn():
+            raise
+
+        self.assertEqual(sanremo.process(raising_fn).exit_code, 2)
+
+    def test_fetch_website(self):
+        @dataclass
+        class Response:
+            status_code: int
+            text: str
+
+            def raise_for_status(self):
+                if self.status_code != 200:
+                    raise RequestException()
+
+        def fetch(status, text, url):
+            self.assertEqual(url, sanremo.URL)
+            return Response(status, text)
+
+        self.assertEqual(
+            sanremo.fetch_website(get=functools.partial(fetch, 200, "foo")), "foo"
+        )
+
+        with self.assertRaises(RequestException):
+            sanremo.fetch_website(get=functools.partial(fetch, 404, "bar"))
