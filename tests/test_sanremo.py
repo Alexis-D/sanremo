d2e0c195e3ff7b11247487c971b17145e2f81b2c
Alexis Daboville <alexis.daboville@gmail.com>
2024-10-12 21:52:24 +0100

    more toying with test setup

--- tests/test_sanremo.py
+++ tests/test_sanremo.py
@@ -1,42 +1,38 @@
-import functools
-import unittest
-from dataclasses import dataclass
+from unittest.mock import Mock
 
+import requests
 import sanremo
+from assertpy import assert_that
 from requests.exceptions import RequestException
 
 
-class TestSanRemo(unittest.TestCase):
-    def test_is_san_remo_interesting_yet(self):
-        self.assertTrue(sanremo.is_san_remo_interesting_yet("<h2>Yes</h2>"))
-        self.assertFalse(sanremo.is_san_remo_interesting_yet("<h2>No</h2>"))
+def test_is_san_remo_interesting_yet():
+    assert_that(sanremo.is_san_remo_interesting_yet("<h2>Yes</h2>")).is_true()
+    assert_that(sanremo.is_san_remo_interesting_yet("<h2>No</h2>")).is_false()
 
-    def test_process(self):
-        self.assertEqual(sanremo.process(lambda: "<h2>Yes</h2>").exit_code, 0)
-        self.assertEqual(sanremo.process(lambda: "<h2>No</h2>").exit_code, 1)
 
-        def raising_fn():
-            raise
+def test_process():
+    assert_that(sanremo.process(lambda: "<h2>Yes</h2>").exit_code).is_equal_to(0)
+    assert_that(sanremo.process(lambda: "<h2>No</h2>").exit_code).is_equal_to(1)
+    assert_that(sanremo.process(Mock(side_effect=Exception)).exit_code).is_equal_to(2)
 
-        self.assertEqual(sanremo.process(raising_fn).exit_code, 2)
 
-    def test_fetch_website(self):
-        @dataclass
-        class Response:
-            status_code: int
-            text: str
+def test_fetch_website_when_get_succeeds():
+    success_reponse = Mock(spec=requests.Response)
+    success_reponse.text = "foo"
+    get_success = Mock(return_value=success_reponse)
+    assert_that(sanremo.fetch_website(get=get_success)).is_equal_to("foo")
+    get_success.assert_called_once_with(sanremo.URL)
+    success_reponse.raise_for_status.assert_called_once()
 
-            def raise_for_status(self):
-                if self.status_code != 200:
-                    raise RequestException()
 
-        def fetch(status, text, url):
-            self.assertEqual(url, sanremo.URL)
-            return Response(status, text)
+def test_fetch_website_when_get_fails():
+    failure_response = Mock(spec=requests.Response)
+    failure_response.raise_for_status.side_effect = RequestException
+    get_failure = Mock(return_value=failure_response)
 
-        self.assertEqual(
-            sanremo.fetch_website(get=functools.partial(fetch, 200, "foo")), "foo"
-        )
-
-        with self.assertRaises(RequestException):
-            sanremo.fetch_website(get=functools.partial(fetch, 404, "bar"))
+    assert_that(sanremo.fetch_website).raises(RequestException).when_called_with(
+        get=get_failure
+    )
+    get_failure.assert_called_once_with(sanremo.URL)
+    failure_response.raise_for_status.assert_called_once()
